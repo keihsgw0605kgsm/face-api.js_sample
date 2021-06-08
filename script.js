4bee7809d8299568d8412ddedca0b4a3ba7c63d7
hsgwKei <63716217+hsgwKei@users.noreply.github.com>
2021-06-09 01:14:29 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -46,10 +46,10 @@ player.addEventListener('play', () => {
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(player, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
     //const detections = await faceapi.detectAllFaces(player, new faceapi.SsdMobilenetv1Options()).withFaceLandmarks()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    //const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    //canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    //faceapi.draw.drawDetections(canvas, resizedDetections)
+    //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     //faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
 
     //結果の出力
