5e77e97568e190c667ff9bb2d313f9cae2ac0f83
hsgwKei <63716217+hsgwKei@users.noreply.github.com>
2021-06-25 07:57:08 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const player = document.getElementById('video')
 const download = document.getElementById('download')
-const p_text = document.getElementById('test')
+//const p_text = document.getElementById('test')
 var detections_json = "No Data"
 const modelUrl = './weights'
 var test_csv = []
@@ -58,12 +58,12 @@ player.addEventListener('play', () => {
     //faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
 
     detections_json = JSON.stringify(detections, null, '\t');
-    p_text.textContent = detections[0]['landmarks']['_positions'][1]['_x']
+    //p_text.textContent = detections[0]['landmarks']['_positions'][1]['_x']
 
-    /*test_csv = [
+    test_csv = [
       ['_x', '_y', '_width', '_height', 'x0', 'y0', 'x1', 'y1'],
       [detections[0]['detection']['_box']['_x'], detections[0]['detection']['_box']['_y'], detections[0]['detection']['_box']['_width'], detections[0]['detection']['_box']['_height'], detections[0]['landmarks']['_positions'][0]['_x'], detections[0]['landmarks']['_positions'][0]['_y'], detections[0]['landmarks']['_positions'][1]['_x'], detections[0]['landmarks']['_positions'][1]['_y']]
-    ]*/
+    ]
 
 
     //結果の出力
@@ -86,12 +86,12 @@ player.addEventListener('play', () => {
 }*/
 
 function handleDownload() {
-  let data = test_csv.map((arr)=>arr.json(',')).json('\r\n');
+  let data = test_csv.map((arr)=>arr.json('\t')).json('\r\n');
   var bom = new Uint8Array([0xEF, 0xBB, 0xBF])
 
   var blob = new Blob([ bom, data ], { "type" : "text/csv" });
   //var url = window.URL.createObjectURL(blob);
   let url = (window.URL || window.webkitURL).createObjectURL(blob);
   download.href = url;
-  //window.navigator.msSaveBlob(blob, "test_face.csv");
+  window.navigator.msSaveBlob(blob, "test_face.csv");
 }
