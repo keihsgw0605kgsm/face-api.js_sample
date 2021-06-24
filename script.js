0db125e4bddb09b4986c51919e638eda88c1a67f
hsgwKei <63716217+hsgwKei@users.noreply.github.com>
2021-06-25 06:53:10 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const player = document.getElementById('video')
+const download = document.getElementById('download')
+const p_text = document.getElementById('test')
+var detections_json = "No Data"
 const modelUrl = './weights'
 
 /**モデルのロード**/
@@ -6,7 +9,7 @@ Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri(modelUrl),
   //faceapi.nets.ssdMobilenetv1.loadFromUri(modelUrl),
   faceapi.nets.faceLandmark68Net.loadFromUri(modelUrl),
-  faceapi.nets.faceRecognitionNet.loadFromUri(modelUrl),
+  //faceapi.nets.faceRecognitionNet.loadFromUri(modelUrl),
   //faceapi.nets.faceExpressionNet.loadFromUri(modelUrl)
 ])
 .catch((e) => {
@@ -38,18 +41,22 @@ function startVideo() {
 
 /**カメラオン時のイベント**/
 player.addEventListener('play', () => {
-  //const canvas = faceapi.createCanvasFromMedia(player)
-  //document.body.append(canvas)
-  //const displaySize = { width: player.width, height: player.height }
-  //faceapi.matchDimensions(canvas, displaySize)
+  const canvas = faceapi.createCanvasFromMedia(player)
+  document.body.append(canvas)
+  const displaySize = { width: player.width, height: player.height }
+  faceapi.matchDimensions(canvas, displaySize)
   
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(player, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
+    detections_json = JSON.stringify(detections, null, '\t');
+    p.textContent = "1"//detections_json['detection']['_box']['_x']
+
+    
     //const detections = await faceapi.detectAllFaces(player, new faceapi.SsdMobilenetv1Options()).withFaceLandmarks()
-    //const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    //canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    //faceapi.draw.drawDetections(canvas, resizedDetections)
-    //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     //faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
 
     //結果の出力
@@ -62,3 +69,11 @@ player.addEventListener('play', () => {
 .catch((e) => {
   console.log('player.addEventListenerでエラー：'+e);
 })
+
+/** jsonファイルのダウンロード **/
+function handleDownload() {
+  var blob = new Blob([ detections_json ], { "type" : "text/plain" });
+  var url = window.URL.createObjectURL(blob);
+  download.href = url;
+  window.navigator.msSaveBlob(blob, "test_face.json"); 
+}
